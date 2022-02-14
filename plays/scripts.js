cf9c6307102c9bc5f938431cc36c42bf7e4e534f
Rolando Hernández <rolandohernandezleal@gmail.com>
2022-02-14 02:39:24 -0300

    add the secret stash

--- plays/scripts.js
+++ plays/scripts.js
@@ -1,11 +1,20 @@
 (function () {
-    $('.menu-item').on('click', function () {
-        $('.menu-item').removeClass('is-active');
+    // Menu
+    $('.button').on('click', function () {
+        $('.button').removeClass('is-active');
         $(this).addClass('is-active');
         $('.collection').removeClass('is-active');
-        $($(this).data('collection')).addClass('is-active'); 
+        $($(this).data('collection')).addClass('is-active');
+
+        // Hidden backlog
+        if ($(this).hasClass('heading')) {
+            $(this).addClass('is-backlog');
+            $(this).text('Backlog');
+            $('.subheading, .menu').hide();
+        }
     });
 
+    // Player
     var player = document.querySelector('#player');
     var games = document.querySelectorAll('.game');
 
