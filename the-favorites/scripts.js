243e55e2cfea444c3df4b652b9748e3264037adf
Rolando Hernández <rolandohernandez@Rolandos-Air-2.attlocal.net>
2022-10-27 00:38:54 -0400

    add changes to the favorites

--- the-favorites/scripts.js
+++ the-favorites/scripts.js
@@ -15,28 +15,4 @@
             }
         }
     });
-
-    // Player
-    var player = document.querySelector('#player');
-    var games = document.querySelectorAll('.game');
-
-    games.forEach(game => {
-        game.onclick = function () {
-            var dataAudio = this.getAttribute('data-audio');
-            if (player.getAttribute('data-playing') != dataAudio) {
-                player.setAttribute('data-playing', dataAudio);
-                player.src = 'audios/' + dataAudio;
-            }
-
-            var activeEl = document.querySelector('.game.is-active');
-            if (activeEl) activeEl.classList.remove('is-active');
-
-            if (player.paused) {
-                player.play();
-                this.classList.add('is-active');
-            } else {
-                player.pause()
-            }
-        };
-    });
 })();
